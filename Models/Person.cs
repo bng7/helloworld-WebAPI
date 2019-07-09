8561ddfb3a2491f7b041f6b67bb81ba49b5d79d9
Ng <bng7@student.monash.edu>
2019-07-09 16:45:58 +1000

    initial commit

---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+namespace helloworld_WebAPI.Models
+{
+  public class Person
+  {
+    public long Id { get; set; },
+    public string FirstName { get; set; },
+    public string LastName { get; set; }
+  }
+}
---
+++ Models/PersonContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace helloworld_WebAPI.Models
+{
+  public class PersonContext : DbContext
+  {
+    
+  }
+}
